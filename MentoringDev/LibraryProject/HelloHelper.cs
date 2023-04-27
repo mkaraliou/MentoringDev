c4184c33210979cf52ca4e2e41c0c9efa4c14218
Mikalai Karaliou <MKaraliou@hamiltongroupuk.com>
2023-04-27 12:45:27 +0300

    Start application. Add console part

---
+++ MentoringDev/LibraryProject/HelloHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryProject
+{
+    public class HelloHelper
+    {
+        public static string CreateHelloPhrase(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                return $"{DateTime.Now:G} Hello, {username}!";
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect args count.");
+            }
+        }
+    }
+}
---
+++ MentoringDev/MentoringConsole/MainConsole.cs
@@ -0,0 +1,21 @@
+﻿using LibraryProject;
+
+namespace MentoringConsole
+{
+    public class MainConsole
+    {
+        public static void Main(string[] args)
+        {
+            VerifyArgs(args);
+            Console.WriteLine(HelloHelper.CreateHelloPhrase(args[0]));
+        }
+
+        private static void VerifyArgs(string[] args)
+        {
+            if (!(args.Count() == 1))
+            {
+                throw new ArgumentException("Incorrect args count");
+            }
+        }
+    }
+}
