cf0f4629e2e7acbe3a3af362885321224808d72b
Mikalai Karaliou <MKaraliou@hamiltongroupuk.com>
2023-05-04 14:00:34 +0300

    Add Task 2

---
+++ Task 2/Task1/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product : IEquatable<Product>
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Equals(Product other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Name == other.Name && Price == other.Price;
+        }
+    }
+}
