51d5e3520fbe4403dc0d36a8b225ae8068ab572c
Mikalai Karaliou <MKaraliou@hamiltongroupuk.com>
2023-07-27 03:55:17 +0300

    Delete TAsk 5 and TAsk 8 from Task9

--- Task8_SQL/Task8_Database/Task8_Database/Tables/Address.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Address]
-(
-	[Id] INT NOT NULL PRIMARY KEY Identity, 
-    [Street] NVARCHAR(50) NOT NULL, 
-    [City] NVARCHAR(50) NULL, 
-    [State] NVARCHAR(50) NULL, 
-    [ZipCode] NVARCHAR(50) NULL
-)
--- Task8_SQL/Task8_Database/Task8_Database/Views/EmployeeInfo.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE VIEW [dbo].[EmployeeInfo]
-	AS 
-	SELECT e.Id as EmployeeId,
-		   p.LastName + ' ' + p.FirstName as EmployeeFullName,
-		   a.ZipCode + ' ' + a.State + ' ' + a.City + ' ' + a.Street as EmployeeFullAddress, 
-		   e.CompanyName + ' ' + e.Position as EmployeeCompanyInfo
-	From dbo.Employee e
-	Left Join dbo.Person p ON e.PersonId = p.Id
-	Left Join dbo.Address a ON e.AddressId = a.Id
